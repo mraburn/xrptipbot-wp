13c4b7c92c609930b31b583d64e216e1739ad2b8
mraburn <mraburn@gmail.com>
2018-06-30 08:43:54 -0500

    Create wp_functions_xrptipbot.php

---
+++ wp_functions_xrptipbot.php
@@ -0,0 +1,65 @@
+// Begin Wordpress function.php code for XRP Tipbot //
+// Add the following to /wp-content/themes/themename/functions.php
+// Afterwards, be sure to Edit your Profile and Add your Twitter handle
+// Also be sure to set an Author to the posts you want to display the Tip Button by editing/updating a post
+
+// Allow Adding Twitter Account to User/Author Profile
+
+function wpb_new_contactmethods( $contactmethods ) {
+
+	return $contactmethods;
+
+}
+
+// Adds the Twitter Field to the Author Profile Admin Area (/wp-admin/profile.php)
+
+add_filter('user_contactmethods','wpb_new_contactmethods',10,1);
+
+// Generate XRP Tipbot Code with Twitter Handle
+
+function wp_author_xrp_tipbot_box( $content ) {
+ 
+	global $post;
+ 
+// Detect if it is a single post with a post author
+
+	if ( is_single() && isset( $post->post_author ) ) {
+ 
+// Check if author has a twitter account in their profile
+
+	if ( get_the_author_meta('twitter') ) {
+	
+	$twitter = get_the_author_meta('twitter', $post->post_author);
+ 
+// Display XRP Tipbot Button
+	
+	// You can add/adjust these parameters to the <a ...> tag.
+	// amount=".50" ... adjust the top amount
+	// size="275" ... adjust button size
+	// unique="true" ... to prevent users to be able to click the Tip button for the second time.
+	// label="Give me" ... to change the text before tipping (the amount is appended after this string)
+	// labelpt="Woohooo " ... to change the text after tipping (the amount is appended after this string)
+	
+	
+	$tipbot_details .= '<a amount="0.25" size="275" to="' . $twitter . '" network="twitter" href="https://www.xrptipbot.com" target="_blank"></a>
+	<script async src="https://www.xrptipbot.com/static/donate/tipper.js" charset="utf-8"></script>';
+ 
+	} 
+ 
+// Pass info to post content
+
+	$content = $content . '<footer class="xrptipbot" >' . $tipbot_details . '</footer>';
+	// Customize the area around the button (margins, padding, etc) of class .xrptipbot in the custom.css of your theme
+	}
+	return $content;
+	}
+ 
+// Add our function to the post content filter 
+
+add_action( 'the_content', 'wp_author_xrp_tipbot_box' );
+ 
+// Allow HTML in author bio section 
+
+remove_filter('pre_user_description', 'wp_filter_kses');
+
+//  END XRP Tip Bot Function Code //
